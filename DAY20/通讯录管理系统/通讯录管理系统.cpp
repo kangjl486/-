b6f570b4abae84ad5253d5a31c1928392248abb4
kangjl486 <1156818909@qq.com>
2026-03-28 21:08:17 +0800

    第一个完整项目

---
+++ DAY20/通讯录管理系统/通讯录管理系统.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+//封装菜单界面
+void showmunu()
+{
+	cout << "**********************" << endl;
+	cout << "*****1.添加联系人*****" << endl;
+	cout << "*****2.显示联系人*****" << endl;
+	cout << "*****3.删除联系人*****" << endl;
+	cout << "*****4.查找联系人*****" << endl;
+	cout << "*****5.修改联系人*****" << endl;
+	cout << "*****6.清空联系人*****" << endl;
+	cout << "*****7.推出通讯录*****" << endl;
+	cout << "**********************" << endl;
+}
+
+
+
+
+
+
+int main()
+{
+	//调用菜单
+	showmunu();
+
+
+
+
+
+	system("pause");
+	return 0;
+}
